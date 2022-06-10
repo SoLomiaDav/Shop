721e6258b17ad884b84bfb06ec563478241b3cb0
SoLomiaDav <92159435+SoLomiaDav@users.noreply.github.com>
2022-06-10 20:37:47 +0300

    models and interfaces

---
+++ Book/Book/Data/Interfaces/IAllBooks.cs
@@ -0,0 +1,15 @@
+﻿using Book.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Book.Data.Interfaces
+{
+    public interface IAllBooks
+    {
+        IEnumerable<Books> Books { get; }
+        IEnumerable<Books> getFavBooks { get; set; }
+        Books getObjectBook(int bookId);
+    }
+}
---
+++ Book/Book/Data/Models/Books.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Book.Data.Models
+{
+    public class Books
+    {
+        public int id { get; set; }
+
+        public string name { get; set; }
+
+        public string author { get; set; }
+
+        public string shortDesc { get; set; }
+
+        public string img { get; set; }
+
+        public ushort price { get; set; }
+
+        public bool isFavorite { get; set; }
+
+        public bool available { get; set; }
+
+        public int categoryID { get; set; }
+
+        public virtual Category Category { get; set; }
+    }
+}
